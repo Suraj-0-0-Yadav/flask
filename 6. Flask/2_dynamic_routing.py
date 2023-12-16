4d41973c67c47a2ce457f1d7bce882467ff970f0
Suraj Yadav <qwertypad7612@gmail.com>
2023-12-16 16:03:04 +0530

    Flask dynamic routing

---
+++ 6. Flask/2_dynamic_routing.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")  # 127.0.0.1:5000
+def index():
+    return "<h1>Hello World<h1/>"
+
+@app.route("/information")  # 127.0.0.1:5000/information
+def information():
+    return "<h1>Get information here !!!<h1/>"
+
+
+@app.route("/puppy/<name>")
+def puppy(name):
+    return f"<h1>This is page for {name.upper()}<h1/>"
+
+if __name__ == "__main__":
+    app.run()
